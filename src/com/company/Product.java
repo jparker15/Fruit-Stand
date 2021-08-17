91212b76337a07597b44499cebb18dca073a2b25
Jeremy <jeremyeparker@yahoo.com>
2021-08-17 19:57:17 -0400

    constructors

--- src/com/company/Product.java
+++ src/com/company/Product.java
@@ -1,2 +1,20 @@
-package com.company;public class Product {
+package com.company;
+
+public class Product {
+
+    public String name;
+    public int price;
+    public int quantity;
+    public int wholeSale;
+
+    public Product(String name){
+        this.name = name;
+    }
+
+    public Product(String name, int price, int quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.wholeSale = price * quantity;
+    }
 }
