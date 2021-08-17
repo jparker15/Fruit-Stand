261170d46540786742746f58af54bea15aee2aa3
Jeremy <jeremyeparker@yahoo.com>
2021-08-17 19:56:40 -0400

    methods, fields, change tossInventory

--- src/com/company/Store.java
+++ src/com/company/Store.java
@@ -1,2 +1,37 @@
-package com.company;public class Store {
+package com.company;
+
+import java.util.ArrayList;
+
+public class Store {
+    public int balance;
+    public ArrayList<Product> inventory = new ArrayList<>();
+    public Console c;
+
+    public Store(int balance){
+        this.balance = balance;
+    }
+
+    public void addInventory (Product p){
+
+        if(balance >= p.wholeSale){
+            inventory.add(p);
+            System.out.println("added "+p.quantity + " " + p.name + " to inventory");
+            balance -= p.wholeSale;
+            System.out.println("New Balance: " + balance);
+        }else{
+            System.out.println("Insufficient Funds");
+        }
+    }
+
+    public void tossInventory (Product p){
+        if(inventory.contains(p)){
+            inventory.remove(p);
+            System.out.println(p.name + "'s has been thrown away");
+        }else{
+            System.out.println("Product not found in inventory");
+        }
+
+    }
+
+
 }
