de4915a2839d535967abd1969f4ec79031dc32a7
Jeremy <jeremyeparker@yahoo.com>
2021-08-17 19:56:13 -0400

    test methods and scanner

---
+++ src/com/company/Console.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+
+public class Console {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void greeting(){
+        System.out.println("LMOAS");
+    }
+
+    public void add (){
+
+    }
+
+
+}
