27b6633f5e841b69b0f36a5d45f27c931801d0d8
Jeremy <jeremyeparker@yahoo.com>
2021-08-17 19:57:01 -0400

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Console console = new Console();
+        Store store = new Store(1000000000);
+
+        console.greeting();
+
+        store.addInventory(new Product("Mango",20,100));
+
+        store.tossInventory(new Product("Mango"));
+
+
     }
 }
