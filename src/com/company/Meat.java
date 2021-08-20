cd69bb6ce2194b3dab3bd8a8ccae81750367526c
Jeremy <jeremyeparker@yahoo.com>
2021-08-19 23:58:48 -0400

    sub clas

--- src/com/company/Meat.java
+++ src/com/company/Meat.java
@@ -1,2 +1,20 @@
-package com.company;public class Meat {
+package com.company;
+
+public class Meat extends Product{
+
+    private String expiration;
+
+    public Meat(String name, int price, int quantity){
+        super(name,price,quantity);
+
+    }
+
+    public String getExpiration (){
+        return expiration;
+    }
+
+    public void setExpiration(String expiration){
+        this.expiration = expiration;
+    }
+
 }
