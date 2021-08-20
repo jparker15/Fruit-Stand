a626c3eadd4caeac735b54dbcbb123ae63e5791f
Jeremy <jeremyeparker@yahoo.com>
2021-08-19 23:59:25 -0400

    store

--- src/com/company/Store.java
+++ src/com/company/Store.java
@@ -3,35 +3,41 @@ package com.company;
 import java.util.ArrayList;
 
 public class Store {
-    public int balance;
-    public ArrayList<Product> inventory = new ArrayList<>();
-    public Console c;
+    private int balance;
+    private ArrayList<Product> inventory = new ArrayList<>();
 
     public Store(int balance){
         this.balance = balance;
     }
 
     public void addInventory (Product p){
-
-        if(balance >= p.wholeSale){
+        if(balance >= p.getWholeSale()){
             inventory.add(p);
-            System.out.println("added "+p.quantity + " " + p.name + " to inventory");
-            balance -= p.wholeSale;
+            System.out.println("added "+p.getQuantity() + " " + p.getName() + " to inventory");
+            balance -= p.getWholeSale();
             System.out.println("New Balance: " + balance);
         }else{
             System.out.println("Insufficient Funds");
         }
     }
 
-    public void tossInventory (Product p){
-        if(inventory.contains(p)){
-            inventory.remove(p);
-            System.out.println(p.name + "'s has been thrown away");
-        }else{
-            System.out.println("Product not found in inventory");
-        }
+    public void tossInventory (String name){
+
+        //TODO:change from Product to name, find index()
+//        if(inventory.contains(p)){
+//            inventory.remove(p);
+//            System.out.println(p.getName() + "'s has been thrown away");
+//        }else{
+//            System.out.println("Product not found in inventory");
+//        }
 
     }
 
+    public void displayInventory(){
+        for(Product p: inventory){
+            p.display();
+            p.setPrice(1);
+        }
+    }
 
 }
