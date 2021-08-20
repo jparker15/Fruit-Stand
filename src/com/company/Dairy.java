5a6275a10c938f186febfc591ea312bbba8d475a
Jeremy <jeremyeparker@yahoo.com>
2021-08-19 23:58:40 -0400

    sub

--- src/com/company/Dairy.java
+++ src/com/company/Dairy.java
@@ -1,2 +1,21 @@
-package com.company;public class Dairy {
+package com.company;
+
+public class Dairy extends Product{
+
+    private String expiration;
+
+    public Dairy(String name, int price, int quantity){
+        super(name,price,quantity);
+
+    }
+
+    public String getExpiration (){
+        return expiration;
+    }
+
+    public void setExpiration(String expiration){
+        this.expiration = expiration;
+    }
+
+
 }
