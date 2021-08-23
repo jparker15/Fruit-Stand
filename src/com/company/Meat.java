b70b49626afb70692d9d656326862d76f90c13a6
Jeremy <jeremyeparker@yahoo.com>
2021-08-23 18:19:17 -0400

    change price from int to double

--- src/com/company/Meat.java
+++ src/com/company/Meat.java
@@ -4,17 +4,13 @@ public class Meat extends Product{
 
     private String expiration;
 
-    public Meat(String name, int price, int quantity){
-        super(name,price,quantity);
+    public Meat(String name, double price, int quantity){
+        super(name, (int) price,quantity);
 
     }
 
     public String getExpiration (){
         return expiration;
-    }
-
-    public void setExpiration(String expiration){
-        this.expiration = expiration;
     }
 
 }
