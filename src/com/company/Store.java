e8c7d4729bbe7a8b120af9fedd518055fa2e67c8
Jeremy <jeremyeparker@yahoo.com>
2021-08-23 18:23:38 -0400

    refactor methods

--- src/com/company/Store.java
+++ src/com/company/Store.java
@@ -1,29 +1,69 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Store {
     private int balance;
     private ArrayList<Product> inventory = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
 
     public Store(int balance){
         this.balance = balance;
     }
 
-    public void addInventory (Product p){
-        if(balance >= p.getWholeSale()){
-            inventory.add(p);
-            System.out.println("added "+p.getQuantity() + " " + p.getName() + " to inventory");
-            balance -= p.getWholeSale();
-            System.out.println("New Balance: " + balance);
-        }else{
-            System.out.println("Insufficient Funds");
+    public void addInventory (){
+
+        while(true){
+            System.out.println("What type of product?");
+            System.out.println("Meat (1) or Diary(2)");
+
+            int answer = scanner.nextInt();
+
+            System.out.println("Name?:");
+            String name = scanner.next();
+            System.out.println("Price?:");
+            double price = scanner.nextDouble();
+            System.out.println("Quantity?:");
+            int quantity = scanner.nextInt();
+
+            if(answer == 1 ){
+                System.out.println(name+price+quantity);
+                inventory.add(new Meat(name,price,quantity));
+                break;
+            }
+            if(answer == 2) {
+                System.out.println(name+price+quantity);
+                inventory.add(new Dairy(name,price,quantity));
+                break;
+
+            }
+            else{
+                System.out.println("\nInvalid Input");
+            }
         }
     }
 
-    public void tossInventory (String name){
+    public void tossInventory (){
 
         //TODO:change from Product to name, find index()
+        if(inventory.size() > 0){
+            while(true){
+                System.out.println("Type the name of the product to throw away:");
+                String answer = scanner.next();
+
+                for(Product p: inventory){
+                    if(p.getName().equals(answer)){
+                        inventory.remove(p);
+                    }else{
+                        System.out.println("Product not in Inventory");
+                    }
+                }
+            }
+        }else{
+            System.out.println("No Products in Inventory");
+        }
+
 //        if(inventory.contains(p)){
 //            inventory.remove(p);
 //            System.out.println(p.getName() + "'s has been thrown away");
@@ -34,9 +74,32 @@ public class Store {
     }
 
     public void displayInventory(){
-        for(Product p: inventory){
-            p.display();
-            p.setPrice(1);
+        if(inventory.size() > 0){
+            for(Product p: inventory){
+                p.display();
+            }
+        }else{
+            System.out.println("Inventory Empty");
+        }
+
+    }
+
+    public void sellInventory(){
+        if(inventory.isEmpty()){
+            System.out.println("Inventory Empty");
+        }else{
+            System.out.println("Type Name of Product to Sell");
+            String name = scanner.next();
+            while(true){
+                for(Product p: inventory){
+                    if(p.getName().equals(name)){
+                        System.out.println(p.getQuantity() );
+                        break;
+                    }else{
+                        System.out.println("Product not in inventory!");
+                    }
+                }
+            }
         }
     }
 
